799822f4b7e809d9036172d4e46613c7539f2ba9
Rishi2553 <rishipanneerselvam@gmail.com>
2025-05-29 15:08:16 +0530

    created currency_converter_material_app dart file

---
+++ lib/currency_convrerter_material_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConvrerterMaterialApp extends StatelessWidget{
+  const CurrencyConvrerterMaterialApp({super.key})
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+    'Hello world!!',
+    textDirection: TextDirection.ltr
+    ),
+
+      )
+    );
+  }
+}
