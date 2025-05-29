f2dab9a1319db0450b2ba9b8bacec34053a2662a
Rishi2553 <rishipanneerselvam@gmail.com>
2025-05-29 23:56:25 +0530

    created text field

--- lib/currency_convrerter_material_app.dart
+++ lib/currency_convrerter_material_app.dart
@@ -1,17 +1,59 @@
 import 'package:flutter/material.dart';
 
 class CurrencyConvrerterMaterialApp extends StatelessWidget{
-  const CurrencyConvrerterMaterialApp({super.key})
+  const CurrencyConvrerterMaterialApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    final boder = OutlineInputBorder(
+                borderSide: BorderSide(
+                  width: 2.0,
+                  style: BorderStyle.solid,
+                ),
+                borderRadius: BorderRadius.circular(
+                  5
+                  ),
+              );
+    return  Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
-        child: Text(
-    'Hello world!!',
-    textDirection: TextDirection.ltr
-    ),
-
-      )
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '0',
+          style: const TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+              decoration: InputDecoration( 
+                hintText: 'Plese Enter Amount in USD',
+                hintStyle: const TextStyle(
+                  color: Colors.black,
+                ),
+                prefixIcon: const Icon(Icons.monetization_on),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+            
+                focusedBorder: boder,
+                enabledBorder: boder,
+              ),
+              keyboardType: const TextInputType.numberWithOptions(
+                decimal: true,
+              ),
+            ),
+          ),
+        ],
+      ),
+      ),
     );
   }
 }
